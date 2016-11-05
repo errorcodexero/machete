11bee0ef71076f4e466f152a6225012dcc96402c
software1 <1425sw@gmail.com>
2016-11-05 09:29:11 -0700

    Get it to compile again

--- control/drivebase.cpp
+++ control/drivebase.cpp
@@ -40,7 +40,7 @@ double ticks_to_inches(const int ticks){
 }
 
 #define L_ENCODER_PORTS 0,1
-#define R_ENCODER_PORTS 2,3//2016 mounted backwards
+#define R_ENCODER_PORTS 2,3
 #define C_ENCODER_PORTS 4,5//doesn't exist?
 #define L_ENCODER_LOC 0
 #define R_ENCODER_LOC 1
@@ -244,10 +244,10 @@ void Drivebase::Estimator::update(Time now,Drivebase::Input in,Drivebase::Output
 }
 
 Robot_outputs Drivebase::Output_applicator::operator()(Robot_outputs robot,Drivebase::Output b)const{
-	/*robot.talon_srx[R_MOTOR_LOC_1].power_level = b.r;
+	robot.talon_srx[R_MOTOR_LOC_1].power_level = b.r;
 	robot.talon_srx[R_MOTOR_LOC_2].power_level = b.r;
 	robot.talon_srx[L_MOTOR_LOC_1].power_level = b.l;
-	robot.talon_srx[L_MOTOR_LOC_2].power_level = b.l;*/
+	robot.talon_srx[L_MOTOR_LOC_2].power_level = b.l;
 	robot.pwm[6]=pwm_convert(b.c);
 
 	robot.solenoid[0] = b.piston;
@@ -262,9 +262,8 @@ Robot_outputs Drivebase::Output_applicator::operator()(Robot_outputs robot,Drive
 Drivebase::Output Drivebase::Output_applicator::operator()(Robot_outputs robot)const{
 	//assuming both motors on the same side are set to the same value//FIXME ?
 	return Drivebase::Output{	
-		/*robot.talon_srx[L_MOTOR_LOC_1].power_level,
-		robot.talon_srx[R_MOTOR_LOC_1].power_level,*/
-		0,0,
+		robot.talon_srx[L_MOTOR_LOC_1].power_level,
+		robot.talon_srx[R_MOTOR_LOC_1].power_level,
 		from_pwm(robot.pwm[6]),
 		robot.solenoid[0]
 	};
