d1527273467058e34548fce12f486281b1fd170d
software1 <1425sw@gmail.com>
2016-11-28 19:29:08 -0800

    Make auto_distance.cpp a little easier to read

--- executive/auto_distance.cpp
+++ executive/auto_distance.cpp
@@ -4,22 +4,28 @@
 using namespace std;
 
 Executive Auto_distance::next_mode(Next_mode_info info){
+	if(!info.autonomous) return Executive{Teleop()};
+	
 	Drivebase::Encoder_ticks encoder_differences={
-		#define X(ENCODER) info.status.drive.ticks.ENCODER-initial_encoders.ENCODER
+		#define X(ENCODER) info.status.drive.ticks.ENCODER - initial_encoders.ENCODER
 		X(left),
 		X(right),
 		X(center)
 	};
-	if(!info.autonomous) return Executive{Teleop()};
+	
 	const double TARGET_DISTANCE = 5.0*12.0;//inches
 	const double TOLERANCE = 6.0;//inches
+	
 	motion_profile.set_goal(TARGET_DISTANCE);
-	cout<<"\nencoder_differences:"<<encoder_differences<<"   "<<ticks_to_inches(encoder_differences.left)<<"   "<<TARGET_DISTANCE<<"\n";
+	
+	cout<<"\nencoder_differences:"<<encoder_differences<<"   left(inches):"<<ticks_to_inches(encoder_differences.left)<<"   target(inches):"<<TARGET_DISTANCE<<"\n";
+	
 	if(ticks_to_inches(encoder_differences.left) >= TARGET_DISTANCE-TOLERANCE && ticks_to_inches(encoder_differences.left) <= TARGET_DISTANCE+TOLERANCE){
 		in_range.update(info.in.now,info.in.robot_mode.enabled);
 	}
 	else{
-		in_range.set(2.0);
+		const double IN_RANGE_TIME = 2.0;//seconds - the time that the robot needs to be within a certain distance from the target
+		in_range.set(IN_RANGE_TIME);
 	}
 	if(in_range.done()){
 		return Executive{Teleop()};
@@ -29,16 +35,17 @@ Executive Auto_distance::next_mode(Next_mode_info info){
 
 Toplevel::Goal Auto_distance::run(Run_info info){
 	Toplevel::Goal goals;
-	double power=-motion_profile.target_speed(ticks_to_inches(info.toplevel_status.drive.ticks.left));
-	goals.drive.y=power;
+	double power = -motion_profile.target_speed(ticks_to_inches(info.toplevel_status.drive.ticks.left));//assuming that the left and right encoder values are similar enough
+	goals.drive.y = power;
 	return goals;
 }
 
-bool Auto_distance::operator==(Auto_distance const&)const{ return true; }
+bool Auto_distance::operator==(Auto_distance const&)const{ return true; }//TODO: update with values in that struct
 
 
 #ifdef AUTO_DISTANCE_TEST
 #include "test.h"
+
 int main(){
 	Auto_distance a = {{0,0,0}};
 	test_executive(a);
